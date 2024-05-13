f914580c751318a29cf2ff025c9b67b63beb6a47
shionyan <146417819+shionyan@users.noreply.github.com>
2024-05-13 15:54:06 +0900

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'skyblue', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
+    'skyblue', 'orange', 'pink', 'yellow', 'red', 'green', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
